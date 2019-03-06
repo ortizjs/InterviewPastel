c1a3f53fb86d2d3e20bf2bf2550e60ca2a9436cc
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-03-06 09:16:23 -0800

    Add hiCalendar interviewCake problem

---
+++ hiCalendar.js
@@ -0,0 +1,31 @@
+var hiCal = function(arr) {
+  let sorted = arr.sort((a,b) => a.startTime - b.startTime);
+  let result = [sorted[0]];
+  for (let i = 1; i < sorted.length - 1; i++){
+    let las = result.length - 1;
+    if (sorted[i].startTime < result[las].endTime){
+      result[las].endTime = sorted[i].endTime;
+    }
+    else {
+      result.push({"startTime": sorted[i].startTime, "endTime": sorted[i].endTime})
+    }
+  }
+  return result;
+}
+
+
+
+hiCal(  [
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9,  endTime: 10 },
+]);
+
+//result
+// [
+//     { startTime: 0, endTime: 1 },
+//     { startTime: 3, endTime: 8 },
+//     { startTime: 9, endTime: 12 },
+// ]
